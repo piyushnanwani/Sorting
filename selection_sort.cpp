1e76430084f91d9cae5cd33515be4daf60e01c4f
piyushnanwani <35925360+piyushnanwani@users.noreply.github.com>
2018-02-06 17:17:30 +0530

    Add files via upload

---
+++ selection_sort.cpp
@@ -0,0 +1,50 @@
+// Selection Sort , array implementation , Increasing order. 
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+
+	// Input array;
+	int arr[20], n;
+
+	cin >> n;   // size of array.
+
+	for(int i=0; i<n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	//  Selection sort
+	int loc;
+
+	for(int i=0; i<n; i++)
+	{
+	// for every position i , i have to find smallest element.
+		int smallest = arr[i];
+		loc = i;
+
+		for(int j=i; j<n; j++)
+		{
+			if(smallest > arr[j])
+			{
+				smallest = arr[j];
+				loc = j;
+			}
+		}
+
+		int temp = arr[i];
+		arr[i]  = arr[loc];
+		arr[loc] = temp;
+	} 
+
+	// ouput of sorted array.
+	for(int i=0; i<n; i++){
+		cout << arr[i] << " ";
+	}
+
+
+	return 0;
+}
